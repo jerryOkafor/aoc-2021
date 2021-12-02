7f0ba34ab0a24f4a69eb7fb93e0eb1e750c703e2
Jerry Okafor <jerryhanksokafor@gmail.com>
2021-12-02 08:01:37 +0100

    feat : submit day 1 of advent of code.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,102 @@
+import java.lang.StringBuilder
+
+/**
+ * --- Day 1: Sonar Sweep ---
+ * You're minding your own business on a ship at sea when the overboard alarm goes off! You rush to see if you can help. Apparently, one of the Elves tripped and accidentally sent the sleigh keys flying into the ocean!
+ *
+ * Before you know it, you're inside a submarine the Elves keep ready for situations like this. It's covered in Christmas lights (because of course it is), and it even has an experimental antenna that should be able to track the keys if you can boost its signal strength high enough; there's a little meter that indicates the antenna's signal strength by displaying 0-50 stars.
+ *
+ * Your instincts tell you that in order to save Christmas, you'll need to get all fifty stars by December 25th.
+ *
+ * Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+ *
+ * As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
+ *
+ * For example, suppose you had the following report:
+ *
+ * 199
+ *
+ * 200
+ *
+ * 208
+ *
+ * 210
+ *
+ * 200
+ *
+ * 207
+ *
+ * 240
+ *
+ * 269
+ *
+ * 260
+ *
+ * 263
+ *
+ * This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
+ *
+ * The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
+ *
+ * To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
+ *
+ * 199 (N/A - no previous measurement)
+ *
+ * 200 (increased)
+ *
+ * 208 (increased)
+ *
+ * 210 (increased)
+ *
+ * 200 (decreased)
+ *
+ * 207 (increased)
+ *
+ * 240 (increased)
+ *
+ * 269 (increased)
+ *
+ * 260 (decreased)
+ *
+ * 263 (increased)
+ *
+ * In this example, there are 7 measurements that are larger than the previous measurement.
+ * */
+
+const val INPUT_FILE_NAME = "day1"
 fun main() {
-    fun part1(input: List<String>): Int {
+
+    fun testInputLine(input: List<String>): Int {
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun solveSonarSweep(input: List<String>) {
+        val outPut = StringBuilder()
+        var increasedCount = 0
+
+        for (s in input.indices) {
+            if (s == 0) {
+                outPut.append("${input[s]} (N/A - no previous measurement)\n")
+                continue
+            }
+
+            val previous = input[s - 1].toInt()
+            val current = input[s].toInt()
+
+            if (current > previous) {
+                increasedCount++
+                outPut.append("${input[s]} (increased)\n")
+            } else {
+                outPut.append("${input[s]} (decreased)\n")
+            }
+        }
+
+        println("Measurements that are larger than the previous measurement: $increasedCount")
+        println("Result: \n\n$outPut")
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput(INPUT_FILE_NAME)
+    check(testInputLine(input) == 2000)
+    solveSonarSweep(input)
 }
